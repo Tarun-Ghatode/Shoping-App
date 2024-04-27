9f83e5bd2cc333635b3da2c25925ad4fcf99e8fe
Tarun Ghatode <tarun.ghatode01@gmail.com>
2024-04-27 14:55:39 +0530

    Pushing code till product list, shorting , pagination

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { fetchCount } from './counterAPI';
+import { fetchCount } from './cartAPI';
 
 const initialState = {
   value: 0,
@@ -20,7 +20,7 @@ export const incrementAsync = createAsyncThunk(
   }
 );
 
-export const counterSlice = createSlice({
+export const cartSlice = createSlice({
   name: 'counter',
   initialState,
   // The `reducers` field lets us define reducers and generate associated actions
@@ -48,11 +48,11 @@ export const counterSlice = createSlice({
   },
 });
 
-export const { increment } = counterSlice.actions;
+export const { increment } = cartSlice.actions;
 
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
 export const selectCount = (state) => state.counter.value;
 
-export default counterSlice.reducer;
+export default cartSlice.reducer;
---
+++ src/features/product/productAPI.js
@@ -0,0 +1,64 @@
+// A mock function to mimic making an async request for data
+export function fetchAllProducts() {
+  return new Promise(async (resolve) =>{
+    const response = await fetch('http://localhost:8080/products')
+    const data = await response.json();
+    resolve({data})
+  }  
+  );
+}
+
+export function fetchProductsByFilters(filter,sort, pagination) {
+  // filter = {"category": ["smartphone", "laptops"]}
+  // sort = {_sort: "price"}
+  // pagination = {_page:1,_limit=10} // _page=1&_limit=10
+  // TODO : on server we will support multi values
+  let queryString = '';
+  for(let key in filter){
+    const categoryValues = filter[key];
+    if(categoryValues.length ){
+      const lastCategoryValue = categoryValues[categoryValues.length-1]
+      queryString +=`${key}=${lastCategoryValue}&`  
+    }    
+  }
+  for(let key in sort){
+    queryString +=`${key}=${sort[key]}&`
+  }
+
+  for(let key in pagination){
+    queryString +=`${key}=${pagination[key]}&`
+  }
+  
+  return new Promise(async (resolve) =>{
+    const response = await fetch('http://localhost:8080/products?'+queryString);
+    let data = await response.json();
+    console.log("🚀 ~ returnnewPromise ~ dḁta:", data)
+
+    const totalItems = await response.headers.get('X-Total-Count') || data.items
+    // Tarun did : based on response from api changed the referenced to data 
+    data = data.data
+    // Tarun did : this did
+
+    resolve({data:{products:data, totalItems:+totalItems}})
+  }  
+  );
+}
+
+
+export function fetchCategories() {
+  return new Promise(async (resolve) =>{
+    const response = await fetch('http://localhost:8080/categories')
+    const data = await response.json();
+    resolve({data})
+  }  
+  );
+}
+
+export function fetchBrands() {
+  return new Promise(async (resolve) =>{
+    const response = await fetch('http://localhost:8080/brands')
+    const data = await response.json();
+    resolve({data})
+  }  
+  );
+}
